bad6e5527b393ed67f2453cf43620cfb4b4830b3
Diana Levchenko <wermyt595@gmail.com>
2022-07-24 16:13:52 +0300

    Java Task 1 commit

---
+++ projectJava/task1_create_a_questionnaire/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scName = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String Name = scName.nextLine();
+
+        Scanner scAddress = new Scanner(System.in);
+        System.out.println("Where are you live, " + Name + "?");
+        String Address = scAddress.nextLine();
+
+        System.out.println("Total information about user:");
+        System.out.println("Name of user - " + Name);
+        System.out.println("Address of user - " + Address);
+    }
+}
