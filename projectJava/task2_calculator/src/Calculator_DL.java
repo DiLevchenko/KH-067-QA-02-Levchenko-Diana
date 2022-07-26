6bf79f2ec423c0b464e051fa7e84b071a83830e4
Diana Levchenko <wermyt595@gmail.com>
2022-07-26 22:13:29 +0300

    Module operator added. User dialogue message order fixed.

--- projectJava/task2_calculator/src/Calculator_DL.java
+++ projectJava/task2_calculator/src/Calculator_DL.java
@@ -6,10 +6,11 @@ public class Calculator_DL {
         double num1, num2, result;
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Please, choose an operator: +, -, *, or /");
-            operator = input.next().charAt(0);
+
         System.out.println("Please, enter first number");
             num1 = input.nextDouble();
+        System.out.println("Please, choose an operator: +, -, *, / or %");
+            operator = input.next().charAt(0);
         System.out.println("Please, enter second number");
             num2 = input.nextDouble();
 
@@ -30,6 +31,10 @@ public class Calculator_DL {
                 result = num1 / num2;
                 System.out.println(num1 + " / " + num2 + " = " + result);
                     break;
+            case '%':
+                result = num1 % num2;
+                System.out.println(num1 + " % " + num2 + " = " + result);
+                break;
             default:
                 System.out.println("Invalid operator!");
                     break;
