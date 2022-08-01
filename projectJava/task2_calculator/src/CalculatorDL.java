57d3f0348b0c9a85724eea40f599888260c23099
Diana Levchenko <wermyt595@gmail.com>
2022-08-02 00:14:25 +0300

    Add const. Update  System.out.println to Printf.

--- projectJava/task2_calculator/src/CalculatorDL.java
+++ projectJava/task2_calculator/src/CalculatorDL.java
@@ -3,26 +3,32 @@ import java.util.Scanner;
 public class CalculatorDL {
     static void executeCalculation(double num1, double num2, char operator) {
         double result;
+        final char add = '+';
+        final char sub = '-';
+        final char mul = '*';
+        final char div = '/';
+        final char mod = '%';
+
         switch (operator) {
-            case '+':
+            case add:
                 result = num1 + num2;
-                System.out.println(num1 + " + " + num2 + " = " + result);
+                System.out.printf("%.2f %c %.2f = %.2f%n", num1, add, num2, result);
                 break;
-            case '-':
+            case sub:
                 result = num1 - num2;
-                System.out.println(num1 + " - " + num2 + " = " + result);
+                System.out.printf("%.2f %c %.2f = %.2f%n", num1, sub, num2, result);
                 break;
-            case '*':
+            case mul:
                 result = num1 * num2;
-                System.out.println(num1 + " * " + num2 + " = " + result);
+                System.out.printf("%.2f %c %.2f = %.2f%n", num1, mul, num2, result);
                 break;
-            case '/':
+            case div:
                 result = num1 / num2;
-                System.out.println(num1 + " / " + num2 + " = " + result);
+                System.out.printf("%.2f %c %.2f = %.2f%n", num1, div, num2, result);
                 break;
-            case '%':
+            case mod:
                 result = num1 % num2;
-                System.out.println(num1 + " % " + num2 + " = " + result);
+                System.out.printf("%.2f %c %.2f = %.2f%n", num1, mod, num2, result);
                 break;
             default:
                 System.out.println("Invalid operator!");
@@ -33,7 +39,6 @@ public class CalculatorDL {
     public static void main(String[] args) {
         char operator;
         double num1, num2;
-        boolean repeatLoop;
         Scanner input = new Scanner(System.in);
         do {
             try {
@@ -47,13 +52,12 @@ public class CalculatorDL {
                 executeCalculation(num1, num2, operator);
 
                 System.out.println("To do new calculation enter anything but \'c\'.");
-                repeatLoop = input.next().charAt(0) != 'c';
             } catch (Exception e) {
                 System.out.println(e);
                 System.out.println("Incorrect input value(number required).");
                 break;
             }
-        } while (repeatLoop);
+        } while (input.next().charAt(0) != 'c');
         input.close();
     }
 }
